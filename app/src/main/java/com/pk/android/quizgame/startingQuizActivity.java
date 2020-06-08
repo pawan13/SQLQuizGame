edb660069d92fdad8bbae31d178dbd8d610cf69f
pawansiwakoti <pawanshiwakoti13gmail.com>
2020-06-08 18:26:52 +1000

    Model and contract class created

--- app/src/main/java/com/pk/android/quizgame/startingQuizActivity.java
+++ app/src/main/java/com/pk/android/quizgame/startingQuizActivity.java
@@ -2,7 +2,10 @@ package com.pk.android.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class startingQuizActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class startingQuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_quiz);
+
+       Button startButton = findViewById(R.id.btnStart);
+       startButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               NavigateToActivityQuiz();
+           }
+       });
+    }
+
+    private void NavigateToActivityQuiz() {
+        Intent intent = new Intent(this, QuizActivity.class);
+        startActivity(intent);
     }
 }
