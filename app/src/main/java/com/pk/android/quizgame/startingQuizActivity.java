4712d9ba0345e9f17a8219ac08d7770ecca7a7a4
pawansiwakoti <pawanshiwakoti13gmail.com>
2020-06-08 11:05:34 +1000

    Initial layout is designed

---
+++ app/src/main/java/com/pk/android/quizgame/startingQuizActivity.java
@@ -0,0 +1,14 @@
+package com.pk.android.quizgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class startingQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_quiz);
+    }
+}
